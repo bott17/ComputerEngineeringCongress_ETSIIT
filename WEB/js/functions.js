966200584cd0a1ce5bc1c0cb87b9b63b05941366
Valenz23 <pva_89@hotmail.com>
2015-04-06 20:07:18 +0200

    Añadido formulario con javascript

--- WEB/js/functions.js
+++ WEB/js/functions.js
@@ -1,1 +1,19 @@
-ljf
+function contacto(){
+	var email = document.getElementById('email').value;
+	var mens = '';
+	
+	//expresion regular
+	var Vema = /[\w-\.]{3,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;	
+	
+		
+	if (!(email.match(Vema)))
+		mens += 'Email no valido \n';	
+	
+	
+	if(mens!=''){
+		alert(mens);
+		return false;
+	}
+	alert('Gracias por ponerse en contacto.');
+	return true;
+}
