db3e5ce0695d19c177e82cd2aa8d67d03a1587ec
Valenz23 <pva_89@hotmail.com>
2015-04-08 16:20:26 +0200

    cosas

---
+++ WEB/estructura/footer.php
@@ -0,0 +1,14 @@
+<footer>
+		<p>Nuestros patrocinadores: 
+			<a id="aPatro1" href="http://www.ugr.es/"><img id="patro1" src="images/patro_1.jpg"/></a>
+			<a id="aPatro2" href="http://etsiit.ugr.es/"><img id="patro2" src="images/patro_2.png"/></a>
+			<a id="aPatro3" href="http://www.mediamarkt.es/"><img id="patro3" src="images/patro_3.png"/></a>
+			<a id="aPatro4" href="http://hp.com/es/es/home.html"><img id="patro4" src="images/patro_4.png"/></a>
+			<a id="aPatro5" href="http://twitter.com/"><img id="patro5" src="images/patro_5.png"/></a>
+		</p>
+    	<p>
+            Copyright &copy; Your Company Name. All rights reserved. Designed by <a href="http://www.free-responsive-templates.com" title="free responsive templates">Free Responsive Templates</a>, Validation 
+        <a class="footerLink" href="http://validator.w3.org/check/referer" title="This page validates as HTML5"><abbr title="HyperText Markup Language">HTML5</abbr></a> | 
+    	<a class="footerLink" href="http://jigsaw.w3.org/css-validator/check/referer" title="This page validates as CSS"><abbr title="Cascading Style Sheets">CSS3</abbr></a>
+	</p>
+</footer>
