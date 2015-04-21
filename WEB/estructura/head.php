48cb86aeb133d2f359290c08083ab431b15bbc34
Valenz23 <pva_89@hotmail.com>
2015-04-21 17:41:32 +0200

    head.php terminado

--- WEB/estructura/head.php
+++ WEB/estructura/head.php
@@ -2,15 +2,25 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script type="text/javascript" src="js/functions.js"></script>
-<!--<title>CEIIE - Presentaci&oacute;n</title>-->
 <?php 
 	$seccion=$_REQUEST["seccion"];
 	
 	switch ($seccion) {
-		case 'noseke':
-			echo "<title>Noseke</title>";
+		case 'actividades':
+			echo "<title>Actividades</title>";
+			break;	
+		case 'contacto':
+			echo "<title>Contacto</title>";
+			break;	
+		case 'horarios':
+			echo "<title>Horarios</title>";
+			break;	
+		case 'inscripcion':
+			echo "<title>Inscripci&oacute;n</title>";
+			break;		
+		case 'patrocinanos':
+			echo "<title>CEIIE - Patrocinanos</title>";
 			break;
-		
 		default:
 			echo "<title>CEIIE - Presentaci&oacute;n</title>";
 			break;
