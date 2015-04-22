75b48b12fc63244b70f70aa8b5bfd31008b6790b
Valenz23 <pva_89@hotmail.com>
2015-04-22 19:18:19 +0200

    puestos patrocinadores con las plantillas php

--- WEB/estructura/content.php
+++ WEB/estructura/content.php
@@ -63,6 +63,65 @@ switch ($seccion) {
 		case 'inscripcion':
 			break;		
 		case 'patrocinanos':
+			echo "
+				<div class='mainContent'>
+		        	<h2>Conviertete en Partnert del CEIIE</h2>
+					<p>CEIIE es la mejor oportunidad de darse a conocer dentro del mundo de la tecnología. Aprovechar primera linea 
+						junto a especialistas y usuarios abre las puertas de tu marca y date a conocer al mundo entero.<br><br>
+						
+						Para convertirse en uno de nuestros patrocinadores vaya a la secci&oacute;n <a href='index.php?sec=contacto'>Contacto</a>
+						donde se encuentra la informaci&oacute;n de contacto para patrocinadores. <br><br>				
+						
+						<ul>Hay dos tipos de patrocinador:
+							<li>Gold (50&euro;)</li>
+							<li>Standard (15&euro;)</li>
+						</ul><br><br>
+						
+						El patrocinador <strong>Gold</strong> incluye:<br>
+						Patrocinador de la Cena de gala de clausura del congreso, patrocinador del cóctel de bienvenida, patrocinador 
+						comidas, patrocinador  de las Memorias del congreso, patrocinador Coffe break, patrocinador de pluma del 
+						congreso, patrocinador del block de notas de congreso, patrocinador  del programa de presentaciones, 
+						patrocinador de la página web, patrocinador de la sesión de posters. <br><br>
+						
+						El patrocinador <strong>Standard</strong> incluye:<br>
+						Patrocinador de las Memorias del congreso, patrocinador	comidas, patrocinador Coffe break, patrocinador 
+						de la página web.<br><br>		
+										
+					</p>
+		        </div>
+		        
+		        
+		        <!-- begin .boxes -->
+		        <br class='clearfloat' />
+		        <div>
+		        	<h3> Algunos de nuestros patrocinadores mas importantes</h3>
+		        	<br>
+		        	<div class='box'>
+		            	<div><a href='http://www.granada.org/'><img src='images/granada.jpg' alt=''></a></div>
+		                <a href='http://www.granada.org/'><h2>Granada</h2></a>
+		            </div>
+		            <div class='box'>
+		            	<div><a href='http://etsiit.ugr.es/'><img src='images/etsiit.jpg' alt=''></a></div>
+		                <a href='http://etsiit.ugr.es/'><h2>ETSIIT</h2></a>
+		            </div>
+		            <div class='box'>
+		            	<div><a href='http://www.dell.es/'><img  src='images/Dell-Logo.jpg' alt=''></a></div>
+		                <a href='http://www.dell.es/'><h2>Dell</h2></a>
+		            </div>
+		            <div class='box'>
+		            	<div><a href='http://www.whatsapp.com/'><img src='images/logo-wasap.png' alt=''></a></div>
+		                <a href='http://www.whatsapp.com/'><h2>Whatsapp</h2></a>
+		            </div>
+		            <div class='box'>
+		            	<div><a href='http://www.tid.es/'><img src='images/logo-tid.jpeg' alt=''></a></div>
+		                <a href='http://www.tid.es/'><h2>Telefónica I+D</h2></a>
+		            </div>
+		            <div class='box'>
+		            	<div><a href='index.php?sec=contacto'><img src='images/logo-tu.jpg' alt=''></a></div>
+		                <a href='index.php?sec=contacto'><h2>Tu espacio te espera</h2></a>
+		            </div>
+		            <br class='clearfloat' />
+		        </div> ";
 			break;
 		default:	
 			echo "
