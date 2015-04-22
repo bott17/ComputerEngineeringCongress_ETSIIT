52d695a731b396dccba834264a4213863ee789b7
Valenz23 <pva_89@hotmail.com>
2015-04-22 18:22:08 +0200

    index.php creado, plantillas base hechas

---
+++ WEB/estructura/sidebar.php
@@ -0,0 +1,25 @@
+<div class="sidebar1">
+	<h2>Enlaces de inter&eacute;s</h2>
+    <ul>
+	<?php
+		switch ($seccion) {
+			case 'actividades':
+				break;	
+			case 'contacto':
+				break;	
+			case 'horarios':
+				break;	
+			case 'inscripcion':
+				break;		
+			case 'patrocinanos':
+				break;
+			default:
+				echo "
+	                <li><a href='index.php#Presentacion'>Presentaci&oacute;n del congreso</a></li>
+					<li><a href='index.php#ETSIIT'>¿Qu&eacute es la ETSIIT?</a></li>
+	                <li><a href='index.php#Granada'>Descubre la ciudad de Granada</a></li>";		
+	 			break;		
+ 		}
+?>            	
+	</ul>
+</div>
--- WEB/index.php
+++ WEB/index.php
@@ -1,51 +1,24 @@
 <!DOCTYPE HTML>
-<!--
-Design by Free Responsive Templates
-http://www.free-responsive-templates.com
-Released for free under a Creative Commons Attribution 3.0 Unported License (CC BY 3.0) 
--->
+<?php
+	session_start();
+	$seccion;
+	
+	if(isset($_REQUEST['sec'])){
+		$seccion=$_REQUEST['sec'];
+	}
+	
+?>
 <html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script type="text/javascript" src="js/functions.js"></script>
-<title>CEIIE - Presentaci&oacute;n</title>
-<link href='http://fonts.googleapis.com/css?family=Gentium+Basic' rel='stylesheet' type='text/css'>
-<link href="css/styles.css" rel="stylesheet" type="text/css" />
-</head>
+	
+<!-- para el title de la pagina -->
+<?php include 'estructura/head.php'; ?>
+
 <body>
 <div class="container">
-    <header>
-    	<div id="navheader">
-    		<div id="izq"><h2><a href="./index.html">Congreso de Estudiantes de Ingenier&iacute;a Inform&aacute;tica</a></h2></div>
-    		<div id="der"><p>Zona de Usuario</br><a href="#">(Login)</a></p></div>    	
-    	</div>
-        <div id="navcontainer">
-            <ul id="navlist" class="nav">
-            	<li id="active"><a href="index.html">Presentaci&oacute;n</a></li>
-            	<li><a  href="horarios.html">Horarios</a>
-            		<ul  class="subnavlist">
-		        		<li><a href="horarios.html#dia1">Primer día</a></li>
-		        		<li><a href="horarios.html#dia2">Segundo día</a></li>
-		        		<li><a href="horarios.html#dia3">Tercer día</a></li>
-		        	</ul>
-            	</li>
-		        <li><a class="navlist"  href="actividades.html">Actividades y Talleres</a>
-		        	
-		        	<ul  class="subnavlist">
-		        		<li><a href="actividades.html#actividad1">Visita guiada a la Alhambra</a></li>
-		        		<li><a href="actividades.html#actividad2">Subida a Sierra Nevada</a></li>
-		        	</ul>
-		        	
-		        </li>
-		        
-		        <li><a href="incripcion.html">Inscr&iacute;bete</a></li>
-		        <li><a href="patrocinanos.html">Patroc&iacute;nanos</a></li>
-		        <li><a href="contacto.html">Contacto</a></li>
-			</ul>
-        </div>
-        <br class="clearfloat" />
-    </header>
+	
+	<!-- para el navegador -->
+    <?php include 'estructura/navigator.php'; ?>
+    
     <section>
     	<!-- begin .slider -->
 		<div class="slider"> <!-- Aqui va menu desplazable (poner lider dentro de class -->
@@ -54,137 +27,19 @@ Released for free under a Creative Commons Attribution 3.0 Unported License (CC 
                     <li><img src="images/logo_ugr.gif" alt=""></li>
                 </ul>
             </div>
-        </div>
-		<!-- end .slider -->        
-        <!-- begin .sidebar1 -->
-        <div class="sidebar1">
-       		<h2>Enlaces de inter&eacute;s</h2>
-            <ul>
-            	<li><a href="">¿Qu&eacute es la ETSIIT?</a></li>
-                <li><a href="">Vente a la Alhambra</a></li>
-                <li><a href="">Sube con nosotros a Sierra Nevada</a></li>
-                <li><a href="">...</a></li>
-            </ul>
-        </div>
-        <!-- end .sidebar1 -->
-        <!-- begin .mainContent -->
-        <div class="mainContent">
-        	<h2>Bienvenidos al CEIIE</h2>
-Est&aacute;s en la p&aacute;gina principal del congreso de estudiantes de ingenier&iacute;a inform&aacute;tica en España. El congreso se celebrar&aacute; en la ciudad de <strong>Granada</strong> en la facultad de inform&aacute;tica o <strong>ETSIIT</strong> durante los d&iacute;as <strong>1, 2 y 3 de junio</strong>.<br><br>
-Durante el congreso podr&aacute;s disfrutar de las interesantes <strong>ponencias</strong> de nuestros invitados, de <strong>talleres</strong> y <strong>actividades</strong> (algunas se realizar&aacute;n fuera del recinto del congreso).<br><br>
-Tambi&eacute;n podr&aacute;s disfrutar de la ciudad de Granada durante tu estancia en el Congreso, cuya fecha coincide con la festividad del <strong>Corpus</strong>!!!<br><br>
-Al congreso te puedes inscribir gratuitamente, pero si quieres asistir tendr&aacute;s que abonar el precio de la entrada correspondiente . Hay descuento para la compra anticipada (mas informaci&oacute;n en la secci&oacute;n <strong>Inscr&iacute;bete</strong>).
-</p>
-
-			<h2>ETSIIT <img src="images/etssit.jpeg" alt="no hay"></h2>
-			<p>
-				La Escuela Técnica Superior de Ingenierías Informática y de Telecomunicación (ETSIIT) es un centro docente 
-				universitario perteneciente a la Universidad de Granada, dedicado a la enseñanza, la investigación y el 
-				estudio de las materias relacionadas con la informática, las ciencias de la computación y las 
-				telecomunicaciones. Se sitúa entre las cincuenta mejores escuelas de ingeniería informática del mundo y 
-				es la sexta a nivel europeo.<br><br>
-				Se encuentra en el Campus Universitario de Aynadamar, junto a la Facultad de Bellas Artes y al Centro de 
-				Investigación en Tecnologías de la Información y la comunicación de la UGR (CITIC-UGR).<br><br>
-				En ella se imparten las siguientes titulaciones:<br>
-				Grado en Ingeniería Informática<br>
-    			Grado en Ingeniería de Tecnologías de Telecomunicación<br>
-    			Doble Grado en Ingeniería Informática y Matemáticas<br><br>
-    			
-    			<strong>Como llegar:</strong><br>			
-			</p>
-			
-			<h2>Granada <img src="images/granada_index.jpg" alt="no hay"></h2>
-			<p>
-				Granada es una ciudad y municipio español, capital de la provincia homónima, en la comunidad autónoma de 
-				Andalucía. Está situada en el centro de la comarca Vega de Granada, a una altitud de 680 msnm, en una 
-				amplia depresión intrabética formada por el río Genil y por el piedemonte del macizo más alto de la 
-				península ibérica, Sierra Nevada, que condiciona su clima.<br><br>
-				Granada constituye un núcleo receptor de turismo, debido a sus monumentos y a la cercanía de su estación 
-				de esquí profesional, así como a la zona histórica conocida como La Alpujarra y también a la parte de la 
-				costa granadina conocida como Costa Tropical. De entre sus construcciones históricas, la Alhambra es una 
-				de las más importantes del país, declarada Patrimonio de la Humanidad por la Unesco en 1984, junto con 
-				el jardín del Generalife y el Albaicín. Su catedral está considerada como la primera iglesia renacentista 
-				de España.<br><br>
-				La Universidad de Granada es la cuarta a nivel nacional por número de alumnos y es uno de los destinos más 
-				populares por los universitarios europeos del programa Erasmus.<br><br>
-				
-				<strong>Como llegar:</strong><br><br>
-				<strong>Vuelos nacionales directos a Granada</strong><br><br>
-				Hay vuelos directos entre Granada y Barcelona (VUELING, IBERIA), Palma de Mallorca (IBERIA, AIR EUROPA), 
-				Melilla (IBERIA) y Madrid (IBERIA).<br><br>
-				El aeropuerto de Granada está a 12km del centro de la ciudad. Al llegar, es mejor tomar un taxi desde el 
-				aeropuerto hasta tu alojamiento sin tener que andar con las maletas. El taxi costará unos 28 euros.<br><br>
-				También hay un autobús de aeropuerto que va al centro de Granada. El horario de los autobuses coincide con 
-				todos los vuelos.<br><br>
-				
-				<strong>Llegar a Granada en tren</strong><br><br>				
-				La estación de trenes está en una zona bastante céntrica y está en Avenida de Andaluces que sale de 
-				Avenida de la Constitución. El número de teléfono es 958 27 12 72. Granada no tiene una línea de alta 
-				velocidad todavía.<br><br>
-				El Talgo tarda unas 5 horas y media desde Madrid. Sevilla está a unas 4 horas en tren. La mayoría de 
-				las personas prefieren viajar en autobús en vez de tren cuando vienen desde Málaga o Almería.<br><br>	
-				
-				<strong>Llegar a Granada en Autobús</strong><br><br>
-				Hay muchos autobuses diarios entre Granada y otras grandes ciudades españolas. La compañía de autobuses 
-				más importante es ALSA y se puede comprar billetes online desde la página ALSA. La estación de autobuses 
-				está en las afueras de Granada así que tendrás que tomar un taxi para llegar al centro. 
-				También se puede coger un autobús (Nº 3 ó 33) para llegar al centro.<br><br>
-				
-				Ejemplo llegar desde Madrid<br>
-				Si vienes desde Madrid, tendrás que ir a la "Estación del Sur".
-				El metro más cerca de la "Estación del sur" es Méndez Álvaro. Los autobuses salen cada hora y si hay muchos 
-				viajeros habrá más autobuses en función de la demanda. Lo más fácil es ir a la estación y tomar el próximo 
-				autobús. Si tienes suerte, no tendrás que esperar más de 2 horas para un autobús. Compra tu billete en la 
-				ventanilla o las maquinas de ALSA.					
-			</p>
-        </div>
-        <!-- begin .boxes -->
-        <br class="clearfloat" />
-        <div class="boxes">
-        	<div class="box">
-            	<div><img src="images/granada.jpg" alt=""></div>
-                <h2>Granada</h2>
-                <p>
-                	Informaci&oacuten sobre la ciudad de granada y como llegar a esta.
-                </p>
-                <p><a href="http://www.granadatur.com/">Leer m&aacute;s</a></p>
-            </div>
-            <div class="box">
-            	<div><img src="images/etsiit.jpg" alt=""></div>
-                <h2>ETSIIT</h2>
-                <p>
-                	Informaci&oacuten sobre la ETSIIT y como llegar a esta
-                </p>
-                <p><a href="http://etsiit.ugr.es/">Leer m&aacute;s</a></p>
-            </div>
-            <div class="box">
-            	<div><img src="images/inscribete.jpg" alt=""></div>
-                <h2>Inscr&iacute;base en el congreso</h2>
-                <p>
-                	Cuanto antes mejor, se encontrar&aacute; con un descuento en la entrada.
-                </p>
-                <p><a href="incripcion.html">Leer m&aacute;s</a></p>
-            </div>
-            <br class="clearfloat" />
-        </div>
-        <!-- end .boxes -->
-        <!-- end .mainContent -->
-        <br class="clearfloat" />
+        </div>  
+            
+        <!-- para la barra lateral de enlaces -->
+        <?php include 'estructura/sidebar.php'; ?>
+        
+        <!-- para el contenido de la pagina -->
+        <?php include 'estructura/content.php'; ?>
+        
     </section>
-    <footer>
-		<p>Nuestros patrocinadores: 
-			<a id="aPatro1" href="http://www.ugr.es/"><img id="patro1" src="images/patro_1.jpg"/></a>
-			<a id="aPatro2" href="http://etsiit.ugr.es/"><img id="patro2" src="images/patro_2.png"/></a>
-			<a id="aPatro3" href="http://www.mediamarkt.es/"><img id="patro3" src="images/patro_3.png"/></a>
-			<a id="aPatro4" href="http://hp.com/es/es/home.html"><img id="patro4" src="images/patro_4.png"/></a>
-			<a id="aPatro5" href="http://twitter.com/"><img id="patro5" src="images/patro_5.png"/></a>
-		</p>
-    	<p>
-            Copyright &copy; Your Company Name. All rights reserved. Designed by <a href="http://www.free-responsive-templates.com" title="free responsive templates">Free Responsive Templates</a>, Validation 
-            <a class="footerLink" href="http://validator.w3.org/check/referer" title="This page validates as HTML5"><abbr title="HyperText Markup Language">HTML5</abbr></a> | 
-            <a class="footerLink" href="http://jigsaw.w3.org/css-validator/check/referer" title="This page validates as CSS"><abbr title="Cascading Style Sheets">CSS3</abbr></a>
-        </p>
-    </footer>
+    
+    <!-- para el pie de pagina -->
+    <?php include 'estructura/footer.php';?>
+    
 </div>
 </body>
 </html>
