bd68cd7dd63e1709a4b692b573715f30b82aa9ba
Valenz23 <pva_89@hotmail.com>
2015-05-13 17:00:18 +0200

    script sql

---
+++ WEB/sql/congresoETSIIT.sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 13-05-2015 a las 16:56:57
+-- Versión del servidor: 5.5.41-0ubuntu0.14.04.1
+-- Versión de PHP: 5.5.9-1ubuntu4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `congresoETSIIT`
+--
+CREATE DATABASE IF NOT EXISTS `congresoETSIIT` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `congresoETSIIT`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `UsuarioActividad`
+--
+
+DROP TABLE IF EXISTS `UsuarioActividad`;
+CREATE TABLE IF NOT EXISTS `UsuarioActividad` (
+  `idActividad` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  PRIMARY KEY (`idActividad`,`idUsuario`),
+  KEY `idUsuario` (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `UsuarioHotel`
+--
+
+DROP TABLE IF EXISTS `UsuarioHotel`;
+CREATE TABLE IF NOT EXISTS `UsuarioHotel` (
+  `idHotel` int(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` int(11) NOT NULL,
+  `fechaEntrada` varchar(100) NOT NULL,
+  `fechaSalida` varchar(100) NOT NULL,
+  `TipoHabitacion` varchar(100) NOT NULL,
+  PRIMARY KEY (`idHotel`,`idUsuario`),
+  KEY `idUsuario` (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `actividad`
+--
+
+DROP TABLE IF EXISTS `actividad`;
+CREATE TABLE IF NOT EXISTS `actividad` (
+  `idActividad` int(11) NOT NULL,
+  `denominacion` varchar(50) NOT NULL,
+  `FechaHora` varchar(50) NOT NULL,
+  `descripcion` varchar(10000) NOT NULL,
+  `foto` varchar(100) NOT NULL,
+  `importe` double NOT NULL,
+  PRIMARY KEY (`idActividad`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuota`
+--
+
+DROP TABLE IF EXISTS `cuota`;
+CREATE TABLE IF NOT EXISTS `cuota` (
+  `idCuota` int(11) NOT NULL,
+  `denominacion` varchar(50) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `importe` double NOT NULL,
+  PRIMARY KEY (`idCuota`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Apellidos` varchar(200) NOT NULL,
+  `Centro de trabajo` varchar(200) NOT NULL,
+  `Telefono` int(11) NOT NULL,
+  `Correo` varchar(200) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `UsuarioActividad`
+--
+ALTER TABLE `UsuarioActividad`
+  ADD CONSTRAINT `UsuarioActividad_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `UsuarioActividad_ibfk_1` FOREIGN KEY (`idActividad`) REFERENCES `actividad` (`idActividad`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `UsuarioHotel`
+--
+ALTER TABLE `UsuarioHotel`
+  ADD CONSTRAINT `UsuarioHotel_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
