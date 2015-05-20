316188f7269c290383b1b28a578c33e8e104c495
Valenz23 <pva_89@hotmail.com>
2015-05-20 17:00:40 +0200

    añadidos datos en actividades y cambiados AI en todos los id

--- WEB/sql/congresoETSIIT.sql
+++ WEB/sql/congresoETSIIT.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 13-05-2015 a las 17:09:01
+-- Tiempo de generación: 20-05-2015 a las 16:57:00
 -- Versión del servidor: 5.5.41-0ubuntu0.14.04.1
 -- Versión de PHP: 5.5.9-1ubuntu4.6
 
@@ -61,14 +61,22 @@ CREATE TABLE IF NOT EXISTS `UsuarioHotel` (
 
 DROP TABLE IF EXISTS `actividad`;
 CREATE TABLE IF NOT EXISTS `actividad` (
-  `idActividad` int(11) NOT NULL,
+  `idActividad` int(11) NOT NULL AUTO_INCREMENT,
   `denominacion` varchar(50) NOT NULL,
   `FechaHora` varchar(50) NOT NULL,
   `descripcion` varchar(10000) NOT NULL,
   `foto` varchar(100) NOT NULL,
   `importe` double NOT NULL,
   PRIMARY KEY (`idActividad`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `actividad`
+--
+
+INSERT INTO `actividad` (`idActividad`, `denominacion`, `FechaHora`, `descripcion`, `foto`, `importe`) VALUES
+(1, 'Visita a la Alhambra', '2 julio, 11:30', 'Es un rico complejo palaciego y fortaleza (alcázar o al-qasr ?????) que alojaba al monarca y a la corte del Reino nazarí de Granada. En 2011 se convirtió en el monumento más visitado de España, recibiendo la cifra histórica de 2310764 visitantes. ', './seccion/imagenes/actividades/alhambra/alhambra.jpg', 4.5),
+(2, 'Subida a Sierra Nevada', '3 julio, 7:45', '\r\nSituada en Andalucía, extendiéndose por las provincias de Granada y Almería. Es el macizo montañoso de mayor altitud de toda Europa occidental, después de los Alpes. Su altitud máxima se alcanza en el pico Mulhacén, de 3.482 metros ', './seccion/imagenes/actividades/sierra_nevada/sierra_nevada.jpg', 12);
 
 -- --------------------------------------------------------
 
@@ -78,12 +86,12 @@ CREATE TABLE IF NOT EXISTS `actividad` (
 
 DROP TABLE IF EXISTS `cuota`;
 CREATE TABLE IF NOT EXISTS `cuota` (
-  `idCuota` int(11) NOT NULL,
+  `idCuota` int(11) NOT NULL AUTO_INCREMENT,
   `denominacion` varchar(50) NOT NULL,
   `descripcion` varchar(100) NOT NULL,
   `importe` double NOT NULL,
   PRIMARY KEY (`idCuota`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -93,7 +101,7 @@ CREATE TABLE IF NOT EXISTS `cuota` (
 
 DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE IF NOT EXISTS `usuario` (
-  `idUsuario` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(50) NOT NULL,
   `Apellidos` varchar(200) NOT NULL,
   `Centro de trabajo` varchar(200) NOT NULL,
@@ -103,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `idCuota` int(11) NOT NULL,
   PRIMARY KEY (`idUsuario`),
   KEY `idCuota` (`idCuota`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Restricciones para tablas volcadas
@@ -113,8 +121,8 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 -- Filtros para la tabla `UsuarioActividad`
 --
 ALTER TABLE `UsuarioActividad`
-  ADD CONSTRAINT `UsuarioActividad_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `UsuarioActividad_ibfk_1` FOREIGN KEY (`idActividad`) REFERENCES `actividad` (`idActividad`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `UsuarioActividad_ibfk_1` FOREIGN KEY (`idActividad`) REFERENCES `actividad` (`idActividad`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `UsuarioActividad_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `UsuarioHotel`
