20384192850f675bbb2fcda7747869eb6983144e
Valenz23 <pva_89@hotmail.com>
2015-05-26 10:43:31 +0200

    puesto enlace para programa de ponencias

--- WEB/estructura/sidebarSesion.php
+++ WEB/estructura/sidebarSesion.php
@@ -5,6 +5,7 @@ $link1 = $url;
 $link2 = $url;
 $desc1 = "";
 $desc2 = "";
+$dia = "";
 
 switch ($ponencia) {
 	case 'magiles':
@@ -12,36 +13,42 @@ switch ($ponencia) {
 		$link2 .= "&pon=prince2";
 		$desc1 = "IFML";
 		$desc2 = "Prince2";
+		$dia = "#dia1";
 		break;
 	case 'ifml':
 		$link1 .= "&pon=magiles";
 		$link2 .= "&pon=prince2";
 		$desc1 = "Metodologias agiles";
 		$desc2 = "Prince2";
+		$dia = "#dia1";
 		break;
 	case 'prince2':
 		$link1 .= "&pon=magiles";
 		$link2 .= "&pon=ifml";
 		$desc1 = "Metodologias agiles";
 		$desc2 = "IFML";
+		$dia = "#dia1";
 		break;
 	case 'visrea':
 		$link1 .= "&pon=digi3d";
 		$link2 .= "&pon=reaumen";
 		$desc1 = "Digitalizacion 3D";
 		$desc2 = "Realidad aumentada";
+		$dia = "#dia1";
 		break;
 	case 'digi3d':
 		$link1 .= "&pon=visrea";
 		$link2 .= "&pon=reaumen";
 		$desc1 = "Visualizacion y realismo";
 		$desc2 = "Realidad aumentada";
+		$dia = "#dia1";
 		break;
 	case 'reaumen':
 		$link1 .= "&pon=visrea";
 		$link2 .= "&pon=digi3d";
 		$desc1 = "Visualizacion y realismo";
 		$desc2 = "Digitalizacion 3D";
+		$dia = "#dia1";
 		break;
 	case 'bdmdim':
 		break;
@@ -86,6 +93,9 @@ switch ($ponencia) {
 		<li>
 			<a href=<?php echo $link2; ?>><?php echo $desc2; ?></a>
 		</li>	
+		<li>
+			<a href="index.php?sec=horarios<?php echo $dia; ?>" >Atras</a>
+		</li>
 	</ul>
 
 </div>
