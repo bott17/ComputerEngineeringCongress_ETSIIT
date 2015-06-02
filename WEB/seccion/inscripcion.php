5dbefe988e5f81e54aec94792bd6d70e89023099
Valenz23 <pva_89@hotmail.com>
2015-06-03 01:19:18 +0200

    eliminado el formulario de inscribete

--- WEB/seccion/inscripcion.php
+++ WEB/seccion/inscripcion.php
@@ -15,50 +15,6 @@
 		
 		?>
 	</ul>
-	<form id="formulario" method='post' action='' enctype='x-www-form-urlencoded'>
-		<table>
-			<tr>
-				<td colspan='2'><img class='shadow' src='seccion/imagenes/inscripcion/JoinToday.png' /></td>
-			</tr>
-			<tr>
-				<th>Tu nombre: </th>
-				<td>
-				<input autofocus type='text' value='' placeholder='Pedro Perez Perez' name='name'>
-				</td>
-			</tr>
-			<tr>
-				<th>Email de contacto: </th>
-				<td>
-				<input type='email' value='' placeholder='direccion@correo.es' name='email'/>
-				</td>
-			</tr>
-			<tr>
-				<th>Telefono de contacto: </th>
-				<td>
-				<input type='number' value='' placeholder='619 555 111' name='phone'/>
-				</td>
-			</tr>
-			<tr>
-				<th>Modalidad de inscripción: </th>
-				<td>
-				<select name='opcion'>
-					<option value='1'>1 Día</option>
-					<option value='2' selected>2 Dias</option>
-				</select></td>
-			</tr>
-			<tr>
-				<th>Recibir noticas del evento en tu email de contacto</th>
-				<td>
-				<input type='checkbox' checked />
-				</td>
-			</tr>
-			<tr>
-				<td colspan='2'>
-				<input id='botonForm' type='submit' value='Inscribir' />
-				</td>
-			</tr>
-		</table>
-	</form>
 </div>
 <!-- para las cajas con enlaces -->
 <?php include 'estructura/boxes.php'; ?>
--- WEB/seccion/sidebar/sidebarInscripcion.php
+++ WEB/seccion/sidebar/sidebarInscripcion.php
@@ -1,6 +1,3 @@
 <li>
 	<a href='index.php?sec=inscripcion#precio'>Precio cuotas</a>
 </li>
-<li>
-	<a href='index.php?sec=inscripcion#formulario'>Formulario de inscripcion</a>
-</li>
