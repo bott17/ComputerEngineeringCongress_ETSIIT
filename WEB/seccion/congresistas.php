84a1d9c19474a8708abc9175560f616052ca727a
unknown <bott17@gmail.com>
2015-06-03 02:53:02 +0200

    recuperar todos los datos

--- WEB/seccion/congresistas.php
+++ WEB/seccion/congresistas.php
@@ -9,8 +9,14 @@
 	<?php
 		for($contador = 0; $contador < mysql_num_rows($arrayCongresistas); $contador++) {
 			$congresista = mysql_fetch_assoc($arrayCongresistas); 
-			echo $congresista["Nombre"];
-			
+			echo 
+				"<div class='actividad'>
+					<span>Nombre: " .$congresista["Nombre"]. " " .$congresista["Apellidos"]."</span><br>
+					<span>Centro de trabajo: ".$congresista["Centro de trabajo"]."</span><br>
+					<span>Telefono: " .$congresista["Telefono"]. "</span><br>
+					<span id=correo" .$contador.">Correo electronico: " .$congresista["Correo"]. "</span><br>
+					<span>Tipo de inscripcion: " .$congresista["idCuota"]. " Con precio de: " .$congresista["importe"]. "€</span><br>
+				</div>";
 		}
 	?>
 </div>
