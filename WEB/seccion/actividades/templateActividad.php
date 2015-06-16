d7cdc42815d784255904fad49269abc75dc02427
unknown <bott17@gmail.com>
2015-06-17 00:37:32 +0200

    ruta y fichero unificado para actividades

--- WEB/seccion/actividades/templateActividad.php
+++ WEB/seccion/actividades/templateActividad.php
@@ -1,28 +1,35 @@
 <?php
-$rutaImg="./seccion/imagenes/actividades/";
-$filename;
-switch ($act) {
-	case 'actAlh':
-		$filename = './info/actAlh.txt';
-		$rutaImg.="alhambra/";
-		break;
-	case 'actSierra':
-		$filename='./info/actSierra.txt';
-		$rutaImg.="sierra_nevada/";
-		break;
-}
+$filename = "./info/actividades/actividades.txt";
+$titulo = "Sin actividad";
+$fecha = "Sin actividad";
+$descripcion = "Sin actividad";
+$rutaImg=""; 
 
 $array = explode("\n", file_get_contents($filename));
+// Lectura del contenido del array
+for($contador=0; $contador < count($array); $contador =$contador + 5){
+	
+	// Recuperacion del identificardor, separando lo que hay antes y despues (que seran saltos de linea)
+	$identificador = split("/", $array[$contador]);
+	
+	if(strcmp($identificador[1],$act) ==0){
+		$titulo = $array[$contador+1];
+		$fecha = $array[$contador+2];
+		$descripcion = $array[$contador+3];
+		$rutaImg = $array[$contador+4]; 
+		
+	}
+}
 
 ?>
 
 <div class='mainContent'>
-	<h2 id='tituloContent'><?php echo $array[0]; ?></h2>
-	<h1 id'tituloContent2'><?php echo $array[1]; ?></h1>
-	<img src=<?php $rutaImg.=$array[3]; echo $rutaImg ;?> />
+	<h2 id='tituloContent'><?php echo $titulo; ?></h2>
+	<h1 id'tituloContent2'><?php echo $fecha; ?></h1>
+	<img src=<?php echo "'".$rutaImg."'" ?> />
 	<p>
 		<br />
-		<?php echo $array[2]; ?>
+		<?php echo $descripcion?>
 	</p>
 </div>
 <br class='clearfloat' />
